764133a41be980ad6f099b25979db0b61713c32c
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-06-09 05:29:53 +0000

    DB setup and add mock data

--- database/data.sql
+++ database/data.sql
@@ -3,9 +3,63 @@
 
 -- EXAMPLE:
 
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+ insert into "users"
+   ("email", "password")
+   values
+     ('kasiet@test.com', 'password1');
+
+ insert into "users"
+   ("email", "password")
+   values
+     ('admin@plantpower.com', 'password1');
+
+
+
+
+ insert into "recipes"
+  ("title", "imageUrl", "description", "userId")
+   values
+     ('BRAIN SAVER SALAD {GLUTEN-FREE, DAIRY-FREE, & FAT-FREE}', './public/images/brain-saver-salad.png', 'This healing salad helps minimize, reduce, and cleanse acids out of the brain and cerebrospinal fluid. It also helps create alkalinity within all body systems.
+
+This recipe comes from Brain Saver Protocols, Cleanses & Recipes, the companion book to Medical Medium Brain Saver. These two essential books about our most complex organ—the brain—dive deep into why people all over the world are suffering with mental health and brain-related symptoms and conditions. Brain Saver unveils the why behind more than 100 brain- and nervous system–related symptoms, diseases, and disorders; Brain Saver Protocols, Cleanses & Recipes reveals the truth about how to heal in even more detail (including cleanses, recipes, supplement protocols, and so much more).
+
+Brain Saver Salad
+
+Ingredients:
+1 cup loosely packed parsley leaves, roughly chopped
+1 cup loosely packed cilantro leaves, roughly chopped
+2 cups loosely packed spinach, roughly chopped
+2 cups loosely packed kale leaves, finely chopped
+2 cups loosely packed arugula, roughly chopped
+4 to 6 cups loosely packed red leaf or green leaf lettuce, chopped 1 1⁄2 cups fresh or steamed green peas
+1 1⁄2 cups thinly sliced cucumber
+1 cup sprouts or microgreens
+
+For the dressing:
+1 cup freshly squeezed orange juice
+2 tablespoons freshly squeezed lemon juice
+2 tablespoons raw honey
+1 sprig fresh rosemary, leaves only
+1⁄4 teaspoon cayenne, or more to taste
+
+Directions:
+Combine all the salad ingredients in a bowl. Toss gently and set aside.
+
+Combine the dressing ingredients in a blender and blend until smooth. Strain through a fine-mesh sieve into a small bowl or jug.
+
+Pour the dressing on top of the salad. Toss gently and serve.
+
+Makes 2-3 servings', '1');
+
+
+ insert into "ratings" ("recipeId", "userId", "rating")
+   values ('1','1','5');
+
+  insert into "likes" ("likeId", "userId", "recipeId")
+   values ('1','1','1');
+
+   insert into "favorites" ("favoriteId", "recipeId","userId")
+   values ('1','1','1');
+
+   insert into "comments" ("commentId", "recipeId","userId", "comment")
+   values ('1','1','1', 'This is the best salad I have ever had OMG!');
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,92 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+
+CREATE TABLE "users" (
+	"userId" serial NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "recipes" (
+	"recipeId" serial NOT NULL,
+	"title" TEXT NOT NULL,
+	"imageUrl" TEXT NOT NULL,
+	"description" TEXT NOT NULL,
+	"userId" integer NOT NULL,
+	"createdAt" timestamptz(6) NOT NULL default now(),
+	CONSTRAINT "recipes_pk" PRIMARY KEY ("recipeId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "comments" (
+	"commentId" serial NOT NULL,
+	"recipeId" integer NOT NULL,
+	"userId" integer NOT NULL,
+	"comment" TEXT NOT NULL,
+	"createdAt" timestamptz(6) NOT NULL default now(),
+	CONSTRAINT "comments_pk" PRIMARY KEY ("commentId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "ratings" (
+	"ratingId" serial NOT NULL,
+	"recipeId" int NOT NULL,
+	"userId" int NOT NULL,
+	"rating" TEXT,
+	"createdAt" timestamptz(6) NOT NULL default now(),
+	CONSTRAINT "ratings_pk" PRIMARY KEY ("ratingId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "likes" (
+	"likeId" serial NOT NULL,
+	"userId" int NOT NULL,
+	"recipeId" int NOT NULL,
+	CONSTRAINT "likes_pk" PRIMARY KEY ("likeId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "favorites" (
+	"favoriteId" serial NOT NULL,
+	"recipeId" int NOT NULL,
+	"userId" int NOT NULL,
+	CONSTRAINT "favorites_pk" PRIMARY KEY ("favoriteId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "recipes" ADD CONSTRAINT "recipes_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("recipeId") REFERENCES "recipes"("recipeId");
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+ALTER TABLE "ratings" ADD CONSTRAINT "ratings_fk0" FOREIGN KEY ("recipeId") REFERENCES "recipes"("recipeId");
+ALTER TABLE "ratings" ADD CONSTRAINT "ratings_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+ALTER TABLE "likes" ADD CONSTRAINT "likes_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+ALTER TABLE "likes" ADD CONSTRAINT "likes_fk1" FOREIGN KEY ("recipeId") REFERENCES "recipes"("recipeId");
+
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_fk0" FOREIGN KEY ("recipeId") REFERENCES "recipes"("recipeId");
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
