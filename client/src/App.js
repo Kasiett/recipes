95781cf251d5f8db275dcb46c2b6f3bab955b5fa
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-06-14 16:50:40 +0000

    Finished styling for the header

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { BiSearchAlt } from 'react-icons/bi';
+import { FaUserCircle } from 'react-icons/fa';
 import './App.css';
 
 function App() {
@@ -17,7 +19,46 @@ function App() {
     readServerData();
   }, []);
 
-  return <div className="App">hello Benny!</div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <nav>
+          <h1 className="upper-logo">PlantPower</h1>
+
+          <ul className="nav-bar">
+            <BiSearchAlt className="li-nav-bar icon-search" />
+            <FaUserCircle className="li-nav-bar icon-user" />
+            <li className="li-nav-bar">
+              <a className="log-in" href="#">
+                Log in
+              </a>
+            </li>
+            <li className="li-nav-bar border-left">
+              <a href="#">Popular</a>
+            </li>
+            <li className="li-nav-bar border-left">
+              <a href="#">About Us</a>
+            </li>
+          </ul>
+        </nav>
+        <ul className="sortby">
+          <li>
+            <a href="#">Salads</a>
+          </li>
+          <li>
+            <a href="#">Entree</a>
+          </li>
+          <li>
+            <a href="#">Smoothies</a>
+          </li>
+          <li>
+            <a href="#">Drinks</a>
+          </li>
+        </ul>
+      </div>
+      <div className="log-out-box"> Log out</div>
+    </div>
+  );
 }
 
 export default App;
